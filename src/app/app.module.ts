dde374539aca216a6b7682d4a14221da91ee9f81
Camila Kadi <camilakadi@hotmail.com>
2024-03-09 20:14:36 -0300

    feat: Adiciona mecanismo de data ao componente
    - Implementa um mecanismo de data ao componente para permitir a seleção e manipulação de datas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,21 @@
 import { CurrencyPipe } from '@angular/common';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import {
+  MAT_MOMENT_DATE_ADAPTER_OPTIONS,
+  MAT_MOMENT_DATE_FORMATS,
+  MomentDateAdapter,
+} from '@angular/material-moment-adapter';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatNativeDateModule } from '@angular/material/core';
+import {
+  DateAdapter,
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE,
+  MatNativeDateModule,
+} from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
@@ -13,16 +24,17 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatTableModule } from '@angular/material/table';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import 'moment/locale/pt-br';
 import { AppComponent } from './app.component';
 import { CurrentMonthComponent } from './components/current-month/current-month.component';
 import { FlowDatasComponent } from './components/flow-datas/flow-datas.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { FormFlowComponent } from './components/form-flow/form-flow.component';
 import { HeaderComponent } from './components/header/header.component';
-import { TableFlowComponent } from './components/table-flow/table-flow.component';
-import { BrlCurrencyPipe } from './pipes/currency.pipe';
-import { CurrencyMaskDirective } from './directives/currency-mask.directive';
 import { QuotesComponent } from './components/quotes/quotes.component';
+import { TableFlowComponent } from './components/table-flow/table-flow.component';
+import { CurrencyMaskDirective } from './directives/currency-mask.directive';
+import { BrlCurrencyPipe } from './pipes/currency.pipe';
 
 @NgModule({
   declarations: [
@@ -50,8 +62,19 @@ import { QuotesComponent } from './components/quotes/quotes.component';
     MatCardModule,
     MatDatepickerModule,
     MatNativeDateModule,
+    ReactiveFormsModule,
   ],
-  providers: [CurrencyPipe],
+  providers: [
+    CurrencyPipe,
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+    { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' },
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS],
+    },
+    { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/components/current-month/current-month.component.ts
+++ src/app/components/current-month/current-month.component.ts
@@ -1,8 +1,41 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatDatepicker } from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-current-month',
   templateUrl: './current-month.component.html',
   styleUrls: ['./current-month.component.scss'],
+  providers: [DatePipe],
 })
-export class CurrentMonthComponent {}
+export class CurrentMonthComponent {
+  constructor(private datePipe: DatePipe) {}
+
+  date = new FormControl(new Date());
+
+  chosenMonthHandler(
+    normalizedMonth: any,
+    datepicker: MatDatepicker<FormControl<Date | null>>
+  ) {
+    this.date.setValue(normalizedMonth);
+    datepicker.close();
+  }
+
+  formatDate(date: Date | null): string {
+    const formattedDate = this.datePipe.transform(date, 'MMMM/yyyy') || '';
+    return formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1);
+  }
+
+  advanceMonth() {
+    const newDate = new Date(this.date.value as Date);
+    newDate.setMonth(newDate.getMonth() + 1);
+    this.date.setValue(newDate);
+  }
+
+  goBackMonth() {
+    const newDate = new Date(this.date.value as Date);
+    newDate.setMonth(newDate.getMonth() - 1);
+    this.date.setValue(newDate);
+  }
+}
